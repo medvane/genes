f450fe9829f8b1c8858301f643f1f8585e803feb
Joon Lee <seouri@gmail.com>
2010-05-13 13:32:41 -0400

    removed unused index from Taxonomy

--- db/migrate/20100513135245_create_taxonomies.rb
+++ db/migrate/20100513135245_create_taxonomies.rb
@@ -3,7 +3,6 @@ class CreateTaxonomies < ActiveRecord::Migration
     create_table :taxonomies do |t|
       t.string :scientific_name
     end
-    add_index :taxonomies, :scientific_name
   end
 
   def self.down
