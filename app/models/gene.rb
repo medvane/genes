195b769cdd9c259ea12eac756c9152e19f7d4ab0
Joon Lee <seouri@gmail.com>
2010-05-26 02:23:33 -0400

    refactored homologs review

--- app/models/gene.rb
+++ app/models/gene.rb
@@ -3,7 +3,7 @@ class Gene < ActiveRecord::Base
   has_many :published_genes
   has_many :reviewed_genes, :order => "reviewed_genes.articles_count desc", :include => :review
   has_one :homologene
-  has_many :homologs, :through => :homologene, :include => [:reviewed_genes]
+  has_many :homologs, :through => :homologene
 
   def to_s
     "#{symbol} (#{taxonomy.scientific_name})"
--- app/models/reviewed_gene.rb
+++ app/models/reviewed_gene.rb
@@ -4,5 +4,5 @@ class ReviewedGene < ActiveRecord::Base
   belongs_to :taxonomy
   serialize :article_id_list
 
-  scope :homologs, lambda {|review, gene| where(:review_id => review.id).where(:gene_id => gene.homologs.compact.map {|h| h.id}).order("articles_count desc") }
+  scope :homologs, lambda {|review, gene| where(:review_id => review.id).where(:gene_id => gene.homologs.compact.map {|h| h.id}).includes(:gene).order("articles_count desc") }
 end
