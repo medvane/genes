6709d85c7f97e7cbfb6cde62eb6a7b0382c539cb
Joon Lee <seouri@gmail.com>
2010-05-26 04:14:21 -0400

    added genes by chromosome for human

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,5 +4,11 @@ $(document).ready(function() {
   $("#review_title").keyup(function() {
     var value = $(this).val();
     $("#review_search_term").val(value)
-  })
+  });
+  $("#toggle_genes_by_species").click(function () {
+    $("#genes_by_species").toggle("slow");
+  });
+  $("#toggle_genes_by_chromosome").click(function () {
+    $("#genes_by_chromosome").toggle("slow");
+  });
 });
