873e3967d636501102425867aec0a71d97bbb27e
Joon Lee <seouri@gmail.com>
2010-06-15 18:44:50 -0400

    added species tab

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,17 +4,18 @@ module ApplicationHelper
     prev_offset = nil if prev_offset <= 0
     next_offset = @offset + @per_page
     pagination = []
-    pagination.push(link_to("Beginning", pagination_params(nil))) if @offset > 0
-    pagination.push(link_to("Previous #{@per_page}", pagination_params(prev_offset))) if @offset > @per_page
-    pagination.push(link_to("Next #{@per_page}", pagination_params(next_offset))) if @last_item.nil? or next_offset < @last_item
+    pagination.push(link_to("Beginning", base_params(:o, nil))) if @offset > 0
+    pagination.push(link_to("Previous #{@per_page}", base_params(:o, prev_offset))) if @offset > @per_page
+    pagination.push(link_to("Next #{@per_page}", base_params(:o, next_offset))) if @last_item.nil? or next_offset < @last_item
     content_tag(:div, pagination.join("\n").html_safe, :class => "pagination")
   end
 
-  def pagination_params(offset)
-    p = { :o => offset }
+  def base_params(key, val)
+    p = {}
     p[:q] = params[:q] if params[:q].present?
     p[:s] = params[:s] if params[:s].present?
     p[:t] = params[:t] if params[:t].present?
+    p[key] = val
     return p
   end
 
--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -10,10 +10,26 @@ module ReviewsHelper
     # down triangle; &#x25BC;
     link_name = name.titleize
     link_name += " &#x25BC;" if @sort_key == name
-    link_to(link_name.html_safe, :s => name)
+    link_to(link_name.html_safe, base_params(:s, name))
   end
 
   def sort_column?(name)
     @sort_key == name ? "sorted" : ""
+  end
+
+  def species_tab(review)
+    species = review.reviewed_genes.group(:taxonomy_id).order("count_gene_id desc").count(:gene_id)
+    current_id = params[:t]
+    li = [content_tag(:li, link_to("All species: #{number_with_delimiter(review.genes_count)}", base_params(:t, nil)), :class => "#{params[:t].blank? ? "current" : nil}")]
+    taxonomy_ids = species.keys
+    taxonomy_ids.each do |taxonomy_id|
+      name = truncate(Taxonomy.find(taxonomy_id).scientific_name, :length => 18)
+      genes = number_with_delimiter(species[taxonomy_id])
+      li_class = (params[:t].present? and params[:t].to_i == taxonomy_id) ? "current" : nil
+      li_style = taxonomy_ids.rindex(taxonomy_id) > 4 ? "display: none" : nil
+      li.push(content_tag(:li, link_to("#{name}: #{genes}", base_params(:t, taxonomy_id)), :class => li_class, :style => li_style))
+    end    
+    ul = content_tag(:ul, li.join("\n").html_safe)
+    content_tag(:div, ul.html_safe, :id => "species_tab")
   end
 end
