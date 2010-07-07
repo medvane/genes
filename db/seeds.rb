09cd503143ce9e85666e19bbe8bde34eebc9392f
Joon Lee <seouri@gmail.com>
2010-07-07 08:46:53 -0400

    just 10 diseases for seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-terms = ["Amyotrophic Lateral Sclerosis", "Alzheimer Disease", "Asthma", "Autistic Disorder", "Cancer/genetics", "Crohn Disease", "Glioblastoma", "Inflammatory Bowel Diseases", "Mental Disorders/genetics", "Multiple Sclerosis", "Systemic Lupus Erythematosus", "Autoimmunity"]
+terms = ["Amyotrophic Lateral Sclerosis", "Alzheimer Disease", "Asthma", "Autistic Disorder", "Cancer/genetics", "Crohn Disease", "Inflammatory Bowel Diseases", "Mental Disorders/genetics", "Multiple Sclerosis", "Systemic Lupus Erythematosus"]
 terms.each do |term|
   review = Review.create!(:title => term, :search_term => "#{term}[majr]")
   Delayed::Job.enqueue(CreateReview.new(review.id))
