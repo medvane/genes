5abbe67d2a73875e55dc012189663a49075a218b
Joon Lee <seouri@gmail.com>
2010-07-27 18:18:03 -0400

    add review reset task

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-terms = ["Amyotrophic Lateral Sclerosis", "Alzheimer Disease", "Asthma", "Autistic Disorder", "Cancer/genetics", "Crohn Disease", "Inflammatory Bowel Diseases", "Mental Disorders/genetics", "Multiple Sclerosis", "Systemic Lupus Erythematosus", "Sjogren's Syndrome", "Myocardial Infarction", "Neoplasm Metastasis", "HIV"]
+terms = ["Alzheimer Disease", "Amyotrophic Lateral Sclerosis", "Asthma", "Autistic Disorder", "Cancer/genetics", "Crohn Disease", "Inflammatory Bowel Diseases", "Mental Disorders/genetics", "Multiple Sclerosis", "Systemic Lupus Erythematosus", "Sjogren's Syndrome", "Myocardial Infarction", "Neoplasm Metastasis", "HIV"]
 terms.each do |term|
   review = Review.create!(:title => term, :search_term => "#{term}[majr]")
   Delayed::Job.enqueue(CreateReview.new(review.id))
