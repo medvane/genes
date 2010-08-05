e5fbd5a40cc4b711d138ae9d74475b50a4cae3d6
Joon Lee <seouri@gmail.com>
2010-08-05 13:56:45 -0400

    add scope for go category

--- app/models/reviewed_go.rb
+++ app/models/reviewed_go.rb
@@ -1,4 +1,8 @@
 class ReviewedGo < ActiveRecord::Base
   belongs_to :review
   belongs_to :go
+  
+  scope :components, where("gos.category = 'Component'").joins(:go)
+  scope :functions, where("gos.category = 'Function'").joins(:go)
+  scope :processes, where("gos.category = 'Process'").joins(:go)
 end
