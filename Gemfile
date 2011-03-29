9bbd604fb092e33bd35a3da224f248c267f9e1a1
Joon Lee <seouri@gmail.com>
2011-03-29 03:21:32 -0400

    upgrade to rails 3.0.5

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'http://rubygems.org'
 
-gem 'rails', '3.0.3'
-gem 'mysql'
+gem 'rails', '3.0.5'
+gem 'mysql2'
 gem 'bio'
 gem 'delayed_job', '2.1.1'
 gem 'jquery-rails'
--- db/schema.rb
+++ db/schema.rb
@@ -1,10 +1,11 @@
-# This file is auto-generated from the current state of the database. Instead of editing this file, 
-# please use the migrations feature of Active Record to incrementally modify your database, and
-# then regenerate this schema definition.
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your database schema. If you need
-# to create the application database on another system, you should be using db:schema:load, not running
-# all the migrations from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended to check this file into your version control system.
@@ -95,8 +96,7 @@ ActiveRecord::Schema.define(:version => 20100805185959) do
     t.integer "gene_id"
   end
 
-  add_index "published_genes", ["article_id"], :name => "index_published_genes_on_article_id"
-  add_index "published_genes", ["gene_id"], :name => "index_published_genes_on_gene_id"
+  add_index "published_genes", ["article_id", "gene_id"], :name => "index_published_genes_on_article_id_and_gene_id", :unique => true
 
   create_table "reviewed_genes", :force => true do |t|
     t.integer "review_id"
